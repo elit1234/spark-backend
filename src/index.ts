32ea00756c0735cff3806cea580da60a38462170
elit1234 <elituffley@gmail.com>
2022-09-29 13:01:43 +1300

    CORS fixes

--- src/index.ts
+++ src/index.ts
@@ -22,9 +22,9 @@ app.use(cors({
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'https://localdev.elijames.xyz');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', "true");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
