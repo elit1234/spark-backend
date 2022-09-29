dd530ba8d277944af650e57c2752f2866a49eff4
elit1234 <elituffley@gmail.com>
2022-09-29 14:02:40 +1300

    Cors fixes

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const port = process.env.PORT;
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://localdev.elijames.xyz');
+    res.setHeader('Access-Control-Allow-Origin', 'https://.elijames.xyz');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
